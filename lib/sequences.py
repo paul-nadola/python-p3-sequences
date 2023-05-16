21d0d1ecabc8c9f49220eb3a6636cb625fff6a84
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-05-16 12:18:44 +0300

    Done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    for i in range(2, length):
+        next_number = fibonacci_sequence[i-1] + fibonacci_sequence[i-2]
+        fibonacci_sequence.append(next_number)
+    print(fibonacci_sequence)
+
+
+print_fibonacci(20)
